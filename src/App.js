8c16309cd6730254058e293bfb1026b907eafcae
Fotocopiapapelito <patrick@papelitomalaga.com>
2025-07-29 19:43:34 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+{
+  "name": "papelito-photocopy-app",
+  "version": "0.1.0",
+  "private": true,
+  "dependencies": {
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-scripts": "5.0.1",
+    "react-dropzone": "^14.2.3",
+    "lucide-react": "^0.309.0",
+    "tailwindcss": "^3.4.1"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  },
+  "browserslist": {
+    "production": [">0.2%", "not dead", "not op_mini all"],
+    "development": ["last 1 chrome version", "last 1 firefox version", "last 1 safari version"]
+  }
+}
